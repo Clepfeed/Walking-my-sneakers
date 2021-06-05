be6c2997818c98ed664171b287db495bae39bee5
Alex <81694529+Clepfeed@users.noreply.github.com>
2021-06-05 15:49:05 +0300

    modified:   .vs/Walking_my_sneakers/v16/.suo 	modified:   .vs/Walking_my_sneakers/v16/Browse.VC.db 	modified:   .vs/Walking_my_sneakers/v16/ipch/AutoPCH/45a4547263fe1184/MAIN.ipch 	modified:   Debug/Main.obj 	modified:   Debug/Walking_.fad23b22.tlog/link.read.1.tlog 	modified:   Debug/Walking_my_sneakers.exe 	modified:   Debug/Walking_my_sneakers.ilk 	modified:   Debug/Walking_my_sneakers.log 	modified:   Debug/Walking_my_sneakers.pdb 	modified:   Debug/vc142.idb 	modified:   Debug/vc142.pdb 	modified:   Header.h 	modified:   Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,21 +1,104 @@
 #include "Header.h";
 
+class Player;
+class Puddle;
+class Patch;
+class Void;
+
+int screen[6][4];
+
+class Player
+{
+    int health;
+    int place;
+public:
+    friend void screen_refresh();
+
+    Player() {
+
+    }
+
+};
+
+class Puddle
+{
+    int damage;
+    int place;
+public:
+    friend void screen_refresh();
+
+    Puddle() {
+
+    }
+};
+
+class Patch
+{
+    int heal;
+    int place;
+public:
+    friend void screen_refresh();
+
+    Patch() {
+
+    }
+}; 
+
+class Void
+{
+
+public:
+    friend void screen_refresh();
+
+    Void() {
+
+    }
+
+
+};
+
+
+void screen_refresh() {
+    /*cout << 15 << "\t16" << "\t17\n";
+
+    cout << 12 << "\t13" << "\t16\n";
+
+    cout << 9 << "\t10" << "\t11\n";
+
+    cout << 6 << "\t7" << "\t8\n";
+
+    cout << 3 << "\t4" << "\t5\n";
+
+    cout << 0 << "\t1" << "\t2\n";*/
+
+    for (int i = 0; i < 6; i++)
+    {
+        for (int q = 0; q < 4; q++)
+        {
+            screen[i][q] = q;
+        }
+        screen[i][0] = 6 - i;
+    }
+    for (int i = 0; i < 6; i++)
+    {
+
+        for (int q = 0; q < 4; q++)
+        {
+            if (q != 0) {
+                cout << screen[i][q] << "\t";
+            }
+        }
+        cout << "\n";
+    }
+    
+}
+
+
 void main()
 {
-    setlocale(0, "");
     int button;
 
-    cout << "----" << "\t----" << "\t----\n";
-    cout << "||||" << "\t||||" << "\t||||\n";
-    cout << "----" << "\t----" << "\t----\n";
-
-    cout << "----" << "\t----" << "\t----\n";
-    cout << "||||" << "\t||||" << "\t||||\n";
-    cout << "----" << "\t----" << "\t----\n";
-
-    cout << "----" << "\t----" << "\t----\n";
-    cout << "||||" << "\t||||" << "\t||||\n";
-    cout << "----" << "\t----" << "\t----\n";
+    cout << "Tap to start game";
 
     while (true)
     {
@@ -44,17 +127,7 @@ void main()
             }
             system("CLS");
 
-            cout << "----" << "\t----" << "\t----\n";
-            cout << "||||" << "\t||||" << "\t||||\n";
-            cout << "----" << "\t----" << "\t----\n";
-
-            cout << "----" << "\t----" << "\t----\n";
-            cout << "||||" << "\t||||" << "\t||||\n";
-            cout << "----" << "\t----" << "\t----\n";
-
-            cout << "----" << "\t----" << "\t----\n";
-            cout << "||||" << "\t||||" << "\t||||\n";
-            cout << "----" << "\t----" << "\t----\n";
+            screen_refresh();
 
         }
     }
